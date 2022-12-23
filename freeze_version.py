04f7abc7d8162175024a4a975e386b8a43cfdc8f
Sujata <sujatam@gaussiansolutions.com>
2022-12-23 21:56:31 +0530

    Added pipeline

--- freeze_version.py
+++ freeze_version.py
@@ -5,10 +5,8 @@ import git
 
 from datetime import date
 
-
 project_path = os.path.dirname(os.path.abspath(__file__))
 version_file_path = '{}/version.py'.format(project_path)
-
 
 year = date.today().year % 2000
 month = date.today().month
@@ -19,8 +17,8 @@ sha = repo.head.object.hexsha
 
 version = '%s.dev0+git.%s' % (version_format, repo.git.rev_parse(sha, short=1))
 buildDate = date.today().strftime('%Y-%m-%d')
-if not repo.head.is_detached and repo.active_branch.name == 'master':
-    version = version_format
+
+version = version_format
 
 with open(version_file_path, 'w+') as version_file:
     version_file.write("version = '{}'\n".format(version))
