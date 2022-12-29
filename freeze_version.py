1304c619d0aa0667d910d8b36997c0ba6fb014ff
Sujata <sujatam@gaussiansolutions.com>
2022-12-29 22:19:58 +0530

    Fixed freeze

--- freeze_version.py
+++ freeze_version.py
@@ -2,27 +2,56 @@
 
 import os
 import git
+import json
+import subprocess
 
 from datetime import date
+
+
+def get_last_version() -> str:
+    """Return the version number of the last release."""
+    json_string = (
+        subprocess.run(
+            ["gh", "release", "view", "--json", "tagName"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        .stdout.decode("utf8")
+        .strip()
+    )
+
+    return json.loads(json_string)["tagName"]
+
+
+def bump_patch_number(version_number: str) -> str:
+    """Return a copy of `version_number` with the patch number incremented."""
+    major, minor, patch = version_number.split(".")
+    return f"{major}.{minor}.{int(patch) + 1}"
+
 
 project_path = os.path.dirname(os.path.abspath(__file__))
 version_file_path = '{}/version.py'.format(project_path)
 
-year = date.today().year % 2000
-month = date.today().month
-day_of_month = date.today().day
-version_format = '{}.{}.{}'.format(year, month, day_of_month)
 repo = git.Repo(search_parent_directories=True)
 sha = repo.head.object.hexsha
 
-version = '%s.dev0+git.%s' % (version_format, repo.git.rev_parse(sha, short=1))
-buildDate = date.today().strftime('%Y-%m-%d')
+build_date = date.today().strftime('%Y-%m-%d')
 
-version = version_format
+try:
+    last_version_number = get_last_version()
+except subprocess.CalledProcessError as err:
+    if err.stderr.decode("utf8").startswith("HTTP 404:"):
+        # The project doesn't have any releases yet.
+        version = "0.0.1"
+    else:
+        raise
+else:
+    version = bump_patch_number(last_version_number)
 
 with open(version_file_path, 'w+') as version_file:
     version_file.write("version = '{}'\n".format(version))
     version_file.write("lastCommit = '{}'\n".format(sha))
     version_file.write("lastCommitShort = '{}'\n".format(
         repo.git.rev_parse(sha, short=1)))
-    version_file.write("buildDate = '{}'\n".format(buildDate))
+    version_file.write("buildDate = '{}'\n".format(build_date))
