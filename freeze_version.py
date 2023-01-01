8cf25a3c5ed72cd6f0cb6932c7f6e6edea11224f
Sujata <sujatam@gaussiansolutions.com>
2023-01-02 00:16:39 +0530

    Fixed build

--- freeze_version.py
+++ freeze_version.py
@@ -1,34 +1,7 @@
 #!/usr/bin/env python
-
 import os
 import git
-import json
-import subprocess
-
 from datetime import date
-
-
-def get_last_version() -> str:
-    """Return the version number of the last release."""
-    json_string = (
-        subprocess.run(
-            ["gh", "release", "view", "--json", "tagName"],
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        .stdout.decode("utf8")
-        .strip()
-    )
-
-    return json.loads(json_string)["tagName"]
-
-
-def bump_patch_number(version_number: str) -> str:
-    """Return a copy of `version_number` with the patch number incremented."""
-    major, minor, patch = version_number.split(".")
-    return f"{major}.{minor}.{int(patch) + 1}"
-
 
 project_path = os.path.dirname(os.path.abspath(__file__))
 version_file_path = '{}/version.py'.format(project_path)
@@ -38,16 +11,7 @@ sha = repo.head.object.hexsha
 
 build_date = date.today().strftime('%Y-%m-%d')
 
-try:
-    last_version_number = get_last_version()
-except subprocess.CalledProcessError as err:
-    if err.stderr.decode("utf8").startswith("HTTP 404:"):
-        # The project doesn't have any releases yet.
-        version = "0.0.1"
-    else:
-        raise
-else:
-    version = bump_patch_number(last_version_number)
+version = '0.0.1'
 
 with open(version_file_path, 'w+') as version_file:
     version_file.write("version = '{}'\n".format(version))
--- setup.py
+++ setup.py
@@ -9,32 +9,25 @@ with open(requirements_file) as f:
     content = f.readlines()
 install_requires = [x.strip() for x in content]
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name="python_ms_core",
+    name='python_ms_core',
     version=version,
-    author="Sujata Misra",
-    author_email="sujatam@gaussiansolutions.com",
-    description="Python Boilerplate with cloud interaction",
+    author='Sujata Misra',
+    author_email='sujatam@gaussiansolutions.com',
+    description='Python Boilerplate with cloud interaction',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/TaskarCenterAtUW/TDEI-Python-ms-core",
+    long_description_content_type='text/markdown',
+    url='https://github.com/TaskarCenterAtUW/TDEI-Python-ms-core',
     install_requires=install_requires,
     packages=find_packages(where='src'),
-    # packages=['python_ms_core'],
-    # namespace_packages=['python_ms_core'],
-    # packages={
-    #     "python_ms_core": "src"
-    # },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.9',
-    # ext_modules=[Extension('python_ms_core.src', ['python_ms_core.src.main'])]
-    # py_modules=['python_ms_core'],
     package_dir={'': 'src'},
 )
