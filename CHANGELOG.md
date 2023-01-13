c572f397841005b3f3794854a917a286d42e33dd
Sujata <sujatam@gaussiansolutions.com>
2023-01-13 13:09:22 +0530

    - Added filtered exception handling - Removed unused packages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change log
 
+### 0.0.2
+- Added filtered exception handling
+- Removed unused packages
+
 ### 0.0.1
 - Introduces Topic and subscription methods in Core.
 - Added methods
--- src/python_ms_core/core/resource_errors/__init__.py
+++ src/python_ms_core/core/resource_errors/__init__.py
@@ -12,43 +12,43 @@ class ExceptionHandler:
             try:
                 return fn(*args, **kwargs)
             except azure.servicebus.exceptions.AutoLockRenewFailed as ex:
-                raise BadRequestError(f'AutoLock Renew Failed Error: {ex}')
+                raise BadRequestError(f'AutoLock Renew Failed Error: {ex.message}')
             except azure.servicebus.exceptions.AutoLockRenewTimeout as ex:
-                raise TimeOutError(f'AutoLock Renew Timeout Error: {ex}')
+                raise TimeOutError(f'AutoLock Renew Timeout Error: {ex.message}')
             except azure.servicebus.exceptions.MessageAlreadySettled as ex:
-                raise BadRequestError(f'Message Already Settled Error: {ex}')
+                raise BadRequestError(f'Message Already Settled Error: {ex.message}')
             except azure.servicebus.exceptions.MessageLockLostError as ex:
-                raise BadRequestError(f'Message Lock Lost Error: {ex}')
+                raise BadRequestError(f'Message Lock Lost Error: {ex.message}')
             except azure.servicebus.exceptions.MessageNotFoundError as ex:
-                raise NotFoundError(f'Message Not Found Error: {ex}')
+                raise NotFoundError(f'Message Not Found Error: {ex.message}')
             except azure.servicebus.exceptions.MessageSizeExceededError as ex:
-                raise BadRequestError(f'Message Size Exceeded Error: {ex}')
+                raise BadRequestError(f'Message Size Exceeded Error: {ex.message}')
             except azure.servicebus.exceptions.MessagingEntityAlreadyExistsError as ex:
-                raise ConflictError(f'Message Entity Already Exists Error: {ex}')
+                raise ConflictError(f'Message Entity Already Exists Error: {ex.message}')
             except azure.servicebus.exceptions.MessagingEntityDisabledError as ex:
-                raise UnProcessableError(f'Message Entity Disabled Error: {ex}')
+                raise UnProcessableError(f'Message Entity Disabled Error: {ex.message}')
             except azure.servicebus.exceptions.MessagingEntityNotFoundError as ex:
-                raise NotFoundError(f'Message Entity Not Found Error: {ex}')
+                raise NotFoundError(f'Message Entity Not Found Error: {ex.message}')
             except azure.servicebus.exceptions.OperationTimeoutError as ex:
-                raise TimeOutError(f'Operation Timeout Error: {ex}')
+                raise TimeOutError(f'Operation Timeout Error: {ex.message}')
             except azure.servicebus.exceptions.ServiceBusAuthenticationError as ex:
-                raise UnauthorizedError(f'Unable to Authenticate Service Bus Error: {ex}')
+                raise UnauthorizedError(f'Unable to Authenticate Service Bus Error: {ex.message}')
             except azure.servicebus.exceptions.ServiceBusAuthorizationError as ex:
-                raise ForbiddenError(f'Unable to Authorization Service Bus Error: {ex}')
+                raise ForbiddenError(f'Unable to Authorization Service Bus Error: {ex.message}')
             except azure.servicebus.exceptions.ServiceBusCommunicationError as ex:
-                raise UnProcessableError(f'Service Bus Communication Error: {ex}')
+                raise UnProcessableError(f'Service Bus Communication Error: {ex.message}')
             except azure.servicebus.exceptions.ServiceBusConnectionError as ex:
-                raise ForbiddenError(f'Service Bus Connection Error: {ex}')
+                raise ForbiddenError(f'Service Bus Connection Error: {ex.message}')
             except azure.servicebus.exceptions.ServiceBusError as ex:
-                raise ForbiddenError(f'Service Bus Error: {ex}')
+                raise ForbiddenError(f'Service Bus Error: {ex.message}')
             except azure.servicebus.exceptions.ServiceBusQuotaExceededError as ex:
-                raise UnProcessableError(f'Service Bus Quota Exceeded Error: {ex}')
+                raise UnProcessableError(f'Service Bus Quota Exceeded Error: {ex.message}')
             except azure.servicebus.exceptions.ServiceBusServerBusyError as ex:
-                raise UnProcessableError(f'Service Bus Server Busy Error: {ex}')
+                raise UnProcessableError(f'Service Bus Server Busy Error: {ex.message}')
             except azure.servicebus.exceptions.SessionCannotBeLockedError as ex:
-                raise UnProcessableError(f'Session Cannot Be Locked Error: {ex}')
+                raise UnProcessableError(f'Session Cannot Be Locked Error: {ex.message}')
             except azure.servicebus.exceptions.SessionLockLostError as ex:
-                raise UnProcessableError(f'Session Lock Lost Error: {ex}')
+                raise UnProcessableError(f'Session Lock Lost Error: {ex.message}')
             except ValueError as ex:
                 raise UnProcessableError(f'UnProcessable Error: {ex}')
             except TypeError as ex:
